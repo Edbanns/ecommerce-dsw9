e664e1adf40b7bc8b580c295834c4ce9b2916c2d
Edbanns <edbanns20@gmail.com>
2026-05-12 23:37:46 +0000

    feat: config database.js y app.js completos

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-//require('dotenv').config();
-//const express = require('express');
-//const app     = express();
-//const port    = process.env.PORT || 3000;
 
-//app.get('/', (req, res) => {
- // res.send(`
-   // <h1>Hello World - [Edgardo Gomez]</h1>
-   // <p>La aplicacion funciona en Render.</p>
-   // <p>Puerto: ${port} | Entorno: ${process.env.NODE_ENV || 'development'}</p>
- // `);
-//});
-
-//app.listen(port, () => {
- // console.log(`Servidor corriendo en puerto ${port}`);
-//});
 
 // app.js — reemplaza el Hello World
 require('dotenv').config();
